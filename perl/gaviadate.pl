675305cf83dfd990776823d2daf1d9d3c92d895e
grahn <grahn>
2001-07-08 09:45:26 +0000

    removed dependency on GNU date(1)

--- perl/gaviadate.pl
+++ perl/gaviadate.pl
@@ -1,18 +1,20 @@
 #!/usr/bin/perl -w
 #
-# $Id: gaviadate.pl,v 1.3 2001-06-17 20:49:18 grahn Exp $
+# $Id: gaviadate.pl,v 1.4 2001-07-08 09:45:26 grahn Exp $
 # $Name:  $
 #
 # gaviadate.pl - insert today's date in any empty
 # date: header
 #
-# Requires GNU date from gnu sh-utils.
-#
+
+($q, $q, $q,
+ $mday, $mon, $year,
+ $q, $q, $q) = localtime(time);
 
 while(<>)
 {
     if(/^\s*date\s*:\s+$/) {
-	print `date "+date         : %Y%m%d"`;
+	printf "date         : %04d%02d%02d\n", 1900+$year, 1+$mon, $mday;
     }
     else {
 	print;

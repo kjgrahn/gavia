e43551f7899be1f926bec9598d36792caae449c2
grahn <grahn>
2006-05-01 16:37:26 +0000

    seemingly working, kind of

--- src/contstream.cc
+++ src/contstream.cc
@@ -1,5 +1,5 @@
 /**
- * $Id: contstream.cc,v 1.1 2006-04-30 19:25:11 grahn Exp $
+ * $Id: contstream.cc,v 1.2 2006-05-01 16:37:26 grahn Exp $
  *
  * Copyright (c) 2006 Jörgen Grahn
  * All rights reserved.
@@ -28,15 +28,58 @@
  *
  */
 static const char* rcsid() { rcsid(); return
-"$Id: contstream.cc,v 1.1 2006-04-30 19:25:11 grahn Exp $";
+"$Id: contstream.cc,v 1.2 2006-05-01 16:37:26 grahn Exp $";
 }
 
 #include <iostream>
+#include <cctype>
 #include "contstream.hh"
 
+namespace {
+    /**
+     * True iff s is empty or only contains space and/or TABs.
+     */
+    bool isblank(const std::string& s)
+    {
+	return s.find_first_not_of(" \t")==std::string::npos;
+    }
+
+    /**
+     * True iff s contains characters, and starts with space or TAB.
+     */
+    bool starts_blank(const std::string& s)
+    {
+	return s.size() && (s[0]==' ' || s[0]=='\t');
+    }
+
+    /**
+     * append 's' to 'acc', while replacing any trailing whitespace
+     * of 'acc' and leading whitespace of 's' with a single ' '.
+     */
+    std::string& append(std::string& acc, const std::string& s)
+    {
+	acc += s;
+	return acc;
+    }
+}
 
 void Continuation::getline(std::string& s)
 {
+    while(is_) {
+	std::string tmp;
+	std::getline(is_, tmp);
+
+	if(starts_blank(tmp) && !isblank(tmp)) {
+	    append(acc_, tmp);
+	}
+	else {
+	    s = acc_;
+	    acc_ = tmp;
+	    return;
+	}
+    }
+
+    s = acc_;
 }
 
 

971696cb9ffb600d95316c57e3f6ededb6d3a446
grahn <grahn>
2007-06-24 20:26:37 +0000

    gavia_stat accepts folded lines

--- perl/gavia_stat.pl
+++ perl/gavia_stat.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 #
-# $Id: gavia_stat.pl,v 1.4 2007-06-24 20:10:24 grahn Exp $
+# $Id: gavia_stat.pl,v 1.5 2007-06-24 20:26:37 grahn Exp $
 # $Name:  $
 #
 # gavia_stat.pl - take a book in 'gab' format and output its content
@@ -10,38 +10,72 @@
 #
 # Copyright (c) 2000, 2007 Jörgen Grahn
 # All rights reserved.
+
 use strict;
 
 my ($place, $date, $observers) = ('', '', '');
 
+my @acc = ();
+
 while(<>)
 {
-    chomp;
+    my @aline = ();
+    my $bline = '';
 
-    if(/^place\s*:\s*(.+)/i) {
-
-	$place = $1;
-	$date = "";
-	$observers = "";
+    if(/^\s+\S/) { # SPX
+	push @acc, $_;
+	next unless eof();
+	@aline = @acc; @acc = ();
     }
-    elsif(/^date\s*:\s*(\d+)/i) {
-
-	$date = $1;
+    elsif(/^\s+/) { # SP
+	@aline = @acc; @acc = ();
+	$bline = $_;
     }
-    elsif(/^observers\s*:\s*(.+)/i) {
-
-	$observers = $1;
+    else { # X
+	@aline = @acc;
+	if(eof()) {
+	    $bline = $_;
+	}
+	else {
+	    @acc = ($_);
+	}
     }
-    elsif(/^(time|weather|comments)/i) {
-
-	;
+    my @llines = ();
+    if(@aline) {
+	my $aline = join('', @aline);
+	$aline =~ s/\s*?\n\s+/ /g;
+	push @llines, $aline;
     }
-    elsif(/^\s*\#/) {
-	;
-    }
-    elsif(/\s*([a-zåäö ]+?)\s*:[^:]*:([^:]*:.*$)/i) {
+    push @llines, $bline if $bline;
+    # end line unfolding stuff
 
-	print "$1 : $date : $place : $2 : $observers\n";
+    foreach (@llines) {
+	chomp;
+
+	if(/^place\s*:\s*(.+)/i) {
+
+	    $place = $1;
+	    $date = "";
+	    $observers = "";
+	}
+	elsif(/^date\s*:\s*(\d+)/i) {
+
+	    $date = $1;
+	}
+	elsif(/^observers\s*:\s*(.+)/i) {
+
+	    $observers = $1;
+	}
+	elsif(/^(time|weather|comments)/i) {
+	    ;
+	}
+	elsif(/^\s*\#/) {
+	    ;
+	}
+	elsif(/\s*([a-zåäö ]+?)\s*:[^:]*:([^:]*:.*$)/i) {
+
+	    print "$1 : $date : $place : $2 : $observers\n";
+	}
     }
 }
 

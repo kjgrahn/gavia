4e69ddb189dbf227e0e6cabe509303649f4b69fd
grahn <grahn>
2008-01-03 10:33:34 +0000

    new failing tests, snarfed from prefer

--- src/test/test_cont.cc
+++ src/test/test_cont.cc
@@ -1,5 +1,5 @@
 /*
- * $Id: test_cont.cc,v 1.7 2007-07-02 11:21:29 grahn Exp $
+ * $Id: test_cont.cc,v 1.8 2008-01-03 10:33:34 grahn Exp $
  *
  * Copyright (C) 2006, 2007 Jörgen Grahn.
  * All rights reserved.
@@ -20,6 +20,20 @@ namespace {
 	string s("deadbeef");
 	getline(c, s);
 	return s;
+    }
+
+    void assert_line(Continuation& c,
+		     const int lineno,
+		     const char * const s) {
+	std::string ss;
+	testicle::assert_(getline(c, ss));
+	testicle::assert_eq(ss, s);
+	testicle::assert_eq(c.line(), lineno);
+    }
+
+    void assert_eof(Continuation& c) {
+	std::string s;
+	testicle::assert_(!getline(c, s));
     }
 }
 
@@ -122,5 +136,165 @@ namespace cont {
 	testicle::assert_eq("bar", line(cs));
 	testicle::assert_eq(2, cs.line());
 	testicle::assert_(!cs);
+    }
+}
+
+
+namespace continuation {
+
+    void testA() {
+	istringstream iss("");
+	Continuation cs(iss);
+	std::string s("deadbeef");
+	testicle::assert_(!getline(cs, s));
+	testicle::assert_eq(s, "deadbeef");
+    }
+    void testB() {
+	istringstream iss("\n");
+	Continuation cs(iss);
+	assert_line(cs, 1, "");
+	assert_eof(cs);
+    }
+    void testC() {
+	istringstream iss("%foo");
+	Continuation cs(iss);
+	assert_line(cs, 1, "%foo");
+	assert_eof(cs);
+    }
+    void testD() {
+	istringstream iss("%foo\n");
+	Continuation cs(iss);
+	assert_line(cs, 1, "%foo");
+	assert_eof(cs);
+    }
+    void testE() {
+	istringstream iss("%foo ");
+	Continuation cs(iss);
+	assert_line(cs, 1, "%foo ");
+	assert_eof(cs);
+    }
+    void testF() {
+	istringstream iss("%foo\n"
+			  "%bar\n");
+	Continuation cs(iss);
+	assert_line(cs, 1, "%foo");
+	assert_line(cs, 2, "%bar");
+	assert_eof(cs);
+    }
+    void testG() {
+	istringstream iss("%foo bar\t\n"
+			  "  foo bar\n");
+	Continuation cs(iss);
+	assert_line(cs, 2, "%foo bar foo bar");
+	assert_eof(cs);
+    }
+    void testH() {
+	istringstream iss("%foobar\t\n"
+			  "  foobar \n"
+			  "  foobar \n"
+			  "%foobar\n");
+	Continuation cs(iss);
+	assert_line(cs, 3, "%foobar foobar foobar ");
+	assert_line(cs, 4, "%foobar");
+	assert_eof(cs);
+    }
+    void testI() {
+	istringstream iss("%foobar\t\n"
+			  "  foobar \n"
+			  "\n"
+			  "%foobar\n");
+	Continuation cs(iss);
+	assert_line(cs, 2, "%foobar foobar ");
+	assert_line(cs, 3, "");
+	assert_line(cs, 4, "%foobar");
+	assert_eof(cs);
+    }
+    void testJ() {
+	istringstream iss("%foo\n"
+			  " foo\n"
+			  " foo\n"
+			  " foo\n");
+	Continuation cs(iss);
+	assert_line(cs, 4, "%foo foo foo foo");
+	assert_eof(cs);
+    }
+    void testK() {
+	istringstream iss("%foo     \n"
+			  "%bar\n");
+	Continuation cs(iss);
+	assert_line(cs, 1, "%foo     ");
+	assert_line(cs, 2, "%bar");
+	assert_eof(cs);
+    }
+    void testL() {
+	istringstream iss("%foo \n"
+			  " bar\t\n"
+			  " baz \n"
+			  "%foo \n"
+			  " bar\t\n"
+			  " baz \n"
+			  " \n"
+			  "%foo \n"
+			  " bar\t\n"
+			  " baz \n");
+	Continuation cs(iss);
+	assert_line(cs, 3, "%foo bar baz ");
+	assert_line(cs, 6, "%foo bar baz ");
+	assert_line(cs, 7, " ");
+	assert_line(cs, 10, "%foo bar baz ");
+	assert_eof(cs);
+    }
+    void testM() {
+	istringstream iss("  \n"
+			  "\t\t");
+	Continuation cs(iss);
+	assert_line(cs, 1, "  ");
+	assert_line(cs, 2, "\t\t");
+	assert_eof(cs);
+    }
+    void testN() {
+	istringstream iss("  \n"
+			  "%foo\n"
+			  " bar\n"
+			  " baz\n"
+			  "%bat");
+	Continuation cs(iss);
+	assert_line(cs, 1, "  ");
+	assert_line(cs, 4, "%foo bar baz");
+	assert_line(cs, 5, "%bat");
+	assert_eof(cs);
+    }
+    void testO() {
+	istringstream iss("%foo\n"
+			  " bar\n"
+			  " baz\n"
+			  "%bat\n"
+			  " ben");
+	Continuation cs(iss);
+	assert_line(cs, 3, "%foo bar baz");
+	assert_line(cs, 5, "%bat ben");
+	assert_eof(cs);
+    }
+    void testP() {
+	istringstream iss("%foo\n"
+			  " foo\n"
+			  "%foo\n"
+			  " foo\n");
+	Continuation cs(iss);
+	assert_line(cs, 2, "%foo foo");
+	assert_line(cs, 4, "%foo foo");
+	assert_eof(cs);
+    }
+    void testR() {
+	istringstream iss("%foo\n"
+			  "\n"
+			  "%bar");
+	Continuation cs(iss);
+	assert_line(cs, 1, "%foo");
+	assert_line(cs, 2, "");
+	assert_line(cs, 3, "%bar");
+	assert_eof(cs);
+	assert_eof(cs);
+	assert_eof(cs);
     }
 }

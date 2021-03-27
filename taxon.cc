5e641085f1cd886cde20338c6c41b3b563b34790
Jörgen Grahn <grahn+src@snipabacken.se>
2021-03-27 22:37:44 +0100

    another ranged-for

--- taxon.cc
+++ taxon.cc
@@ -39,10 +39,8 @@ bool Taxon::match(const Regex& re) const
 {
     if(re.match(name)) return true;
     if(re.match(latin)) return true;
-    for(std::vector<std::string>::const_iterator i = alias.begin();
-	i!=alias.end();
-	i++) {
-	if(re.match(*i)) return true;
+    for(const std::string& a : alias) {
+	if(re.match(a)) return true;
     }
     return false;
 }
